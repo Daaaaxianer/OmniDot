2e3e6fa69a391294813c42f6d0af6463af857a05
huwanjie <wanjiehu@163.com>
2026-02-02 16:26:26 +0800

    Add files via upload

--- Intragenic Level/project/src/make_fignew_4.py
+++ Intragenic Level/project/src/make_fignew_4.py
@@ -14,9 +14,11 @@ try:
     # 加载TOML配置文件
     with open('config.toml', "r", encoding="utf-8") as f:
         config = toml.load(f)
-    for var_name in current_vars:# 遍历所有需要检查的变量
-        if var_name in config:
-            globals()[var_name] = config[var_name]
+    if "Intragenic" in config:
+        intragenic_config = config["Intragenic"]
+        for var_name in current_vars:# 遍历所有需要检查的变量
+            if var_name in intragenic_config:
+                globals()[var_name] = intragenic_config[var_name]
 except FileNotFoundError:
     print("⚠ 警告: 未找到 config.toml 文件，使用所有默认配置")
 except Exception as e:
